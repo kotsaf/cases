b661ea56296f5977bd5bcade670f080bb8f60605
kotsaf <safqwer.136@gmail.com>
2024-10-02 00:19:15 +0300

    Add files via upload

---
+++ РЕГ СИСТЕМА НА КЛАССАХ.py
@@ -0,0 +1,38 @@
+class Database:
+    def __init__(self):
+        self. data = {}
+    def add_user(self, username, password):
+         self.data[username] = password
+
+
+class User:
+    """
+   Класс пользователя, содержащий атрибуты: логин, пароль.
+    """
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+if __name__ == '__main__':
+    database = Database()
+    while True:
+        choice = int(input('Выберите действие:\n1-Вход \n2-Регистрация\n'))
+        if choice == 1:
+            login = input('Введите логин: ')
+            password = input('Введите пароль: ')
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен, {login}\n')
+                    break
+                else:
+                    print('Неверный пароль\n')
+            else:
+                print('Пользователь не найден.\n')
+        if choice == 2:
+            user = User(input('Введите логин: '), password := input('Введите пароль: '),
+                        password2 := input('Повторите пароль: '))
+            if password != password2:
+                print('Пароли не совпадают')
+                continue
+            database.add_user(user.username, user.password)
---
+++ РЕГИСТРАЦИЯ ПОСТАВЩИКОВ.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import Tk, RIGHT, BOTH, RAISED
+
+All_objects = []
+
+class Add_dealer(tk.Toplevel):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.label = tk.Label(self, text="Это всплывающее окно")
+        self.btn_close_add_dealer = tk.Button(self, text="Закрыть", command=self.destroy)
+        self.btn_add_dealer_name = tk.Button(
+            self,
+            text="Добавить",
+            command=self.add_new_dealer                             # --- ()
+        )
+        self.txt_field_dealer_name = tk.Entry(self, width=10)
+
+
+        self.txt_field_dealer_name.pack(padx=20, pady=20)
+        self.label.pack(side=RIGHT, padx=20, pady=20)
+        self.btn_add_dealer_name.pack(pady=5, ipadx=2, ipady=2)
+        self.btn_close_add_dealer.pack(pady=5, ipadx=2, ipady=2)
+
+    def add_new_dealer(self):                                       # --- , parent):
+        self.new_dealer_name = self.txt_field_dealer_name.get()
+        All_objects.append(self.new_dealer_name)
+        print(All_objects)
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.btn_open_add_dealer = tk.Button(self, text="Добавить поставщика",
+                                             command=self.open_window_add_dealer)
+        self.btn_open_add_dealer.pack(padx=50, pady=20)
+
+    def open_window_add_dealer(self):
+        window_add_dealer = Add_dealer(self)
+        window_add_dealer.grab_set()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
